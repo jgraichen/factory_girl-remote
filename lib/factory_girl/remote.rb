a134d651051ec70bae6c9171718bb0fbe3b4ef40
Jan Graichen <jg@altimos.de>
2013-10-22 14:13:27 +0200

    Add method to register Rack::Remote handler.

--- lib/factory_girl/remote.rb
+++ lib/factory_girl/remote.rb
@@ -16,7 +16,7 @@ module FactoryGirl
     end
 
     def invoke(method, attrs)
-      Rack::Remote.invoke self.remote, :factory_girl_remote, attrs, { 'Accept' => 'application/json' }
+      Rack::Remote.invoke self.remote, :factory_girl_remote, attrs.merge(_method: method), { 'Accept' => 'application/json' }
     end
 
     class << self
@@ -31,6 +31,15 @@ module FactoryGirl
     [:create, :build, :attributes_for].each do |name|
       define_invoke_method "#{name}"
       define_invoke_method "#{name}_list"
+    end
+
+    class << self
+      def init_server
+        Rack::Remote.register :factory_girl_remote do |params, env, request|
+          method = params.delete(:_method) || :create
+          FactoryGirl.send method, *params
+        end
+      end
     end
   end
 end
